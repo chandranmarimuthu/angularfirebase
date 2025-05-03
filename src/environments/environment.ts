7b83d1b02d01983f3c0e42802e63678ac073d69f
Chandran M <chandru.moutlook.com>
2025-05-03 11:53:05 +0530

    codelab starting point

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyBqzR5beK5OKBhetsrRwTQZdLLhdoo2Rtc',
+    authDomain: 'friendlychat-eed5e.firebaseapp.com',
+    databaseURL: 'https://friendlychat-eed5e.firebaseio.com',
+    projectId: 'friendlychat-eed5e',
+    storageBucket: 'friendlychat-eed5e.appspot.com',
+    messagingSenderId: '988638563061',
+    appId: '1:988638563061:web:f177d45c6a748ce6be13bc',
+    measurementId: 'G-5T5T098M33',
   },
 };
